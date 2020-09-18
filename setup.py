d7eaee8be30d3afde0c24904177f74191d8834f9
soffensive <9149004+soffensive@users.noreply.github.com>
2020-09-18 18:51:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'psycopg2==2.6.1',
         'boto==2.38.0',
         'BeautifulSoup',
-        'flask-migrate==2.0.0'
+        'flask-migrate==2.0.0',
+        'itsdangerous==0.24'
     ]
 )
