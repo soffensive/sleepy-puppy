0ddcc67cc891ef0a26c175829edd537414423855
soffensive <9149004+soffensive@users.noreply.github.com>
2020-09-19 11:34:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,26 +19,5 @@ setup(
     long_description=__doc__,
     packages=['sleepypuppy'],
     include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'Flask==0.10.1',
-        'WTForms==2.0.2',
-        'Flask-Admin==1.2.0',
-        'Flask-Bcrypt==0.6.2',
-        'Flask-Login==0.2.11',
-        'Flask-Mail==0.9.1',
-        'Flask-RESTful==0.3.4',
-        'Flask-SQLAlchemy==2.0',
-        'Flask-Script==2.0.5',
-        'Flask-WTF==0.11',
-        'Pillow==2.8.2',
-        'SQLAlchemy==1.0.5',
-        'bcrypt==2.0.0',
-        'gunicorn==19.3.0',
-        'psycopg2==2.6.1',
-        'boto==2.38.0',
-        'BeautifulSoup',
-        'flask-migrate==2.0.0'
-
-    ]
+    zip_safe=False
 )
